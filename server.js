b75d6a9245d43d88d93d21e811b0db89cb41f20e
Zuzanna Zajkiewicz <machefi1@gmail.com>
2020-11-28 12:05:59 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.get('/users', function(request, response){
     })
 })
 
+app.get('/users/:id', function(request, response){
+    console.log(request.params.id);
+    response.json({
+        success: true,
+        message: 'Got one user. Period.',
+        users: request.params.id
+    })
+})
+
 app.listen(8000, function(){
     console.log("Server is running!");
 });
